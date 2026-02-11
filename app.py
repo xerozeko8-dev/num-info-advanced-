177d93bb75216a4bf38e9e3060e44c905d82f4ad
xerozeko8-dev <xerozeko8@gmail.com>
2026-02-11 06:34:59 -0800

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ HTML_TEMPLATE = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HIMANSHU | STRICT INTEL</title>
+    <title>@None_Usernamz | STRICT INTEL</title>
     <style>
         body { margin: 0; overflow: hidden; background: #000; font-family: 'Courier New', monospace; color: #0f0; }
         #canvas-container { position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: -1; }
